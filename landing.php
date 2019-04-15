be31b2aaf9a59cef200cf1d13f7c2945e7bed43a
NebulaGaming <nebulagaming123@gmail.com>
2019-04-15 17:44:12 -0400

    Create landing.php

---
+++ landing.php
@@ -0,0 +1,24 @@
+<?php
+If(!isset($_SESSION['username'])){
+	echo "<script>window.location = \"start.php\";</script>";
+}
+?>
+<html>
+  <head>
+	<?php include_once('includehead.php'); ?>
+    <title>Start</title>
+  </head>
+  <body>
+    <div class="container">
+		<div class="text-center mt-5" role="group">
+			Landing page<br>
+			Choose desired page
+			<div class="btn-group btn-group-lg mt-3">
+				<a href="namedsearch.php" class="btn btn-secondary">Named Logs (SQL)</a>
+				<a href="numberedsearch.php" class="btn btn-secondary">Numbered Logs (SQL)</a>
+				<a href="ajaxdemo.php" class="btn btn-warning">AJAX Demo (JS)</a>
+			</div>
+		</div>
+    </div>
+  </body>
+</html>
