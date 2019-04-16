6b4c1fca437666650a7066461f8c346e3c8dfccb
NebulaCoding <nebulagaming123@gmail.com>
2019-04-16 12:32:42 -0400

    Updates

--- loginfn.php
+++ loginfn.php
@@ -8,16 +8,16 @@ if($_REQUEST['param'] == "Yes"){
 	
 	$conn = db_connect(true);
 	
-	$qry = $conn->prepare("SELECT EXISTS(SELECT * FROM logins WHERE username=? AND active=? AND password=? AND ipaddr=?) AS Pass");
+	$qry = $conn->prepare("SELECT EXISTS(SELECT * FROM logins WHERE username=? AND active=1 AND password=? AND ipaddr=?) AS Pass");
 	
-	$qry->bind_param("siss", $username, 1, $password, $_SERVER['REMOTE_ADDR']);
+	$qry->bind_param("sss", $username, $password, $_SERVER['REMOTE_ADDR']);
 	
 	$qry->execute();
 	
 	$qry->bind_result($pass);
 	$qry->fetch();
 	
-	$pass['Pass'] == 0 ? Success() : Failure();
+	$pass['Pass'] != 0 ? Login($username, true) : Login($username, false);
 		
 }else{
 	
@@ -28,28 +28,33 @@ if($_REQUEST['param'] == "Yes"){
 	$out = mysqli_query($conn, $qry);
 	$pass = mysqli_fetch_assoc($out);
 	
-	$pass['Pass'] == 0 ? Success() : Failure();
+	$pass['Pass'] != 0 ? Login($username, true) : Login($username, false);
 	
 }
 
-function Success(){
-	$_SESSION['username'] == $username;
-	
-	$qry2 = $conn->prepare("INSERT INTO logintrack (username, success, ipaddr) VALUES(?,?,?)");
-	$qry2->bind_param("sis", $username, 1, $_SERVER['REMOTE_ADDR']);
-	
-	$qry2->execute();
-	
-	echo "<script>window.location = \"landing.php\";</script>";
-}
+function Login( string $username, bool $success ){
+	$conn = db_connect(true);
+	$ip = $_SERVER['REMOTE_ADDR'];
 
-function Failure(){
 	$qry2 = $conn->prepare("INSERT INTO logintrack (username, success, ipaddr) VALUES(?,?,?)");
-	$qry2->bind_param("sis", $username, 0, $_SERVER['REMOTE_ADDR']);
-	
-	$qry2->execute();
-	
-	echo "<strong>Username, password, and IP combination not found for any active users. Please try again.</strong>";
+
+	if($success){
+		$_SESSION['username'] == $username;
+
+		$logSucc = 1;
+		$qry2->bind_param("sis", $username, $logSucc, $ip);
+
+		$qry2->execute();
+
+		echo "<script>window.location = \"landing.php\";</script>";
+	}else{
+		$logSucc = 0;
+		$qry2->bind_param("sis", $username, $logSucc, $ip);
+
+		$qry2->execute();
+
+		echo "<strong>Username, password, and IP combination not found for any active users. Please try again.</strong>";
+	}
 }
 ?>
 
