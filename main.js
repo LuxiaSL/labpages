62fd4829aae45531640553343a71acaeef1f5e16
NebulaGaming <nebulagaming123@gmail.com>
2019-04-17 17:31:37 -0400

    Create main.js

---
+++ main.js
@@ -0,0 +1,25 @@
+function post(destURL, data, callback){
+	let xhttp, formData;
+	xhttp = new XmlHttpRequest();
+	formData = new FormData();
+	
+	xhttp.onreadystatechange = function(){
+		if(this.readyState == 4){
+			if(this.status == 200){
+				callback ? callback(this.responseText, this) : console.log("POST succeeded");
+			}else{
+				console.log(`Something went wrong with the request, response status: ${this.status}, text: ${this.statusText}`);
+			}
+		}
+	};
+	
+	xhttp.open('POST', url, true);
+	
+	for(let entry in data){
+		if(data.hasOwnProperty(entry){
+			formData.append(entry, data[entry]);
+		}
+	}
+	
+	xhttp.send(formData);
+}
