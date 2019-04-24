b9a7e9bd42c77eace3e8e123a6a204f6e86a6389
NebulaGaming <nebulagaming123@gmail.com>
2019-04-24 17:35:42 -0400

    Update landing.php

--- landing.php
+++ landing.php
@@ -1,20 +1,20 @@
 <?php include_once('includeLoginCheck.php'); ?>
 <html>
-  <head>
-	<?php include_once('includehead.php'); ?>
-    <title>Start</title>
-  </head>
-  <body>
-    <div class="container">
-		<div class="text-center mt-5" role="group">
-			Landing page<br>
-			Choose desired page
-			<div class="btn-group btn-group-lg mt-3">
-				<a href="namedsearch.php" class="btn btn-secondary">Named Logs (SQL)</a>
-				<a href="numberedsearch.php" class="btn btn-secondary">Numbered Logs (SQL)</a>
-				<a href="ajaxdemo.php" class="btn btn-warning">AJAX Demo (JS)</a>
+	<head>
+		<?php include_once('includehead.php'); ?>
+    	<title>Start</title>
+  	</head>
+  	<body>
+    	<div class="container">
+			<div class="text-center mt-5" role="group">
+				Landing page<br>
+				Choose desired page
+				<div class="btn-group btn-group-lg mt-3" role="group">
+					<a href="namedsearch.php" class="btn btn-secondary">Named Logs (SQL)</a>
+					<a href="numberedsearch.php" class="btn btn-secondary">Numbered Logs (SQL)</a>
+					<a href="ajaxdemo.php" class="btn btn-warning">AJAX Demo (JS)</a>
+				</div>
 			</div>
-		</div>
-    </div>
-  </body>
+    	</div>
+  	</body>
 </html>
