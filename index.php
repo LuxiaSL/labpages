9edd0655978e9988c23d3be179dacd6f3617a1d6
NebulaGaming <nebulagaming123@gmail.com>
2019-04-24 17:20:50 -0400

    Update index.php

--- index.php
+++ index.php
@@ -4,19 +4,19 @@ If(session_status() == PHP_SESSION_NONE){
 }
 ?>
 <html>
-  <head>
-	<?php include_once('includehead.php'); ?>
-    <title>Start</title>
-  </head>
-  <body>
-    <div class="container">
-		<div class="text-center mt-5" role="group">
-			Choose a login page<br>
-			<div class="btn-group btn-group-lg mt-3">
-				<a href="securelogin.php" class="btn btn-primary">Secure</a>
-				<a href="insecurelogin.php" class="btn btn-primary">Insecure</a>
+	<head>
+		<?php include_once('includehead.php'); ?>
+    	<title>Start</title>
+	</head>
+  	<body>
+    	<div class="container">
+			<div class="text-center mt-5">
+				Choose a login page<br>
+				<div class="btn-group btn-group-lg mt-3" role="group">
+					<a href="securelogin.php" class="btn btn-primary">Secure</a>
+					<a href="insecurelogin.php" class="btn btn-primary">Insecure</a>
+				</div>
 			</div>
-		</div>
-    </div>
-  </body>
+    	</div>
+  	</body>
 </html>
