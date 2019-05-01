5a12d29bfa18fe74d75472179a23405dc3a5008d
NebulaGaming <nebulagaming123@gmail.com>
2019-05-01 11:35:48 -0400

    Update ajaxdemo.php

--- ajaxdemo.php
+++ ajaxdemo.php
@@ -11,7 +11,54 @@
 				<h3>AJAX Demo</h3><br>
 				
 				<p class="demo-explain">
+					The purpose of this page is to show some of the basics of how AJAX works, and some ways it can be used maliciously.<br>
+					The basic priniciple of AJAX is that you can use it to send form data just like with regular HTML forms, but to arbitrary pages.<br>
+					The code doing the AJAX work is complicated, but included below:
+					<pre><code>
+					function post(destURL, data, callback){
+						let xhttp, formData;
+						xhttp = new XMLHttpRequest();
+						formData = new FormData();
+
+						xhttp.onreadystatechange = function(){
+							if(this.readyState == 4){
+								if(this.status == 200){
+									callback ? callback(this.responseText, this) : console.log("POST succeeded");
+								}else{
+									console.log(`Something went wrong with the request, response status: ${this.status}, text: ${this.statusText}`);
+								}
+							}
+						};
+
+						xhttp.open('POST', destURL, true);
+
+						for(let entry in data){
+							if(data.hasOwnProperty(entry)){
+								formData.append(entry, data[entry]);
+							}
+						}
+
+						xhttp.send(formData);
+					}
+					</code></pre>
 				
+					The pseudo-code steps are as follows:<br><br>
+
+					<ol class="list-group">
+						<li class="list-group-item">
+							Create the request object to be sent over POST, and the data object to be sent.
+						</li>
+						<li class="list-group-item">
+							Establish a function that does something with the result of the POST. In this specifically, it passes the result
+							into a function passed by the developer, or logs the error.
+						</li>
+						<li class="list-group-item">
+							Open the POST request to the given site
+						</li>
+						<li class="list-group-item">
+							Generate the data for the forged request, and then send it using the request object.
+						</li>
+					</ol>
 				</p>
 				
 				<div class="form-cont"><br><br>
@@ -51,27 +98,12 @@
         };
 
         function submit(){
-            let fileRes = ValidateEl(htmlInp.fileUp),
-                forgeRes = ValidateEl(htmlInp.searchData),
-                urlRes = ValidateEl(htmlInp.destURL),
-                resDiv = document.getElementById('resDiv'),
+            let resDiv = document.getElementById('resDiv'),
                 resSucc = true;
-
-            if(!fileRes.pass){
-                //do something
-                resSucc = false;
-            }
-
-            if(!forgeRes.pass){
-                //do something
-                resSucc = false;
-            }
-
-            if(!destURL.pass){
-                //do something
-                resSucc = false;
-            }
-
+			ValidateEl(htmlInp.fileUp, false);
+			ValidateEl(htmlInp.searchData, false);
+			ValidateEl(htmlInp.destURL, false);
+		
             if(resSucc){
                 let formObj = {
                     [htmlInp.username.name] : htmlInp.username.value,
